3e4bf5732c0e68e1134c5d24a2b4773a22b26e01
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-25 20:54:54 -0300

    All URL data (origin, protocol, host, etc)

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -4,3 +4,6 @@ const myUrl = new URL('http://google.com?id=100&status=active');
 
 // Serialized URL
 console.log('Serialized URL', myUrl.href);
+
+// All URL data (origin, protocol, host, etc)
+console.log('Serialized URL', myUrl);
