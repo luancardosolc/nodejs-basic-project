72ec628c2fadfe662f6e20f84ee08287f1cd8597
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-25 20:18:30 -0300

    CPU Arch, and Core Info

--- reference/os_demo.js
+++ reference/os_demo.js
@@ -2,3 +2,9 @@ const os = require('os');
 
 // Platform
 console.log('Plataform', os.platform());
+
+// CPU Arch
+console.log('CPU Arch', os.arch());
+
+// Core Info
+console.log('CPU Arch', os.cpus());
