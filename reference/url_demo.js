bf452fee7b4e56a71cca586790574422dcc8fd48
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-25 21:26:39 -0300

    Adding search params

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -7,3 +7,7 @@ console.log('Serialized URL', myUrl.href);
 
 // All URL data (origin, protocol, host, etc)
 console.log('Serialized URL', myUrl);
+
+// Add Params
+myUrl.searchParams.append('abc', '123');
+console.log('Search Params:', myUrl.searchParams);
