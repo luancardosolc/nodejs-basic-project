2f74871202ddd0c70f9a35b3d7fe5c58c91b3420
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-26 17:47:46 -0300

    Response content-type

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+   res.writeHead(200, { 'Content-Type': 'text/html' });
    if (req.url === '/') {
       res.end('<h1>Home</h1>');
    }
