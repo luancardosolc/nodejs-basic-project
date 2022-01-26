9b460ff3b47b823c5cda15a0738d75efbbd807b7
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-26 17:52:42 -0300

    Reading html files and sending their content as response

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-   res.writeHead(200, { 'Content-Type': 'text/html' });
    if (req.url === '/') {
-      res.end('<h1>Home</h1>');
+      fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+         if (err) throw err;
+         res.writeHead(200, { 'Content-Type': 'text/html' });
+         res.end(content);
+      })
    }
    if (req.url === '/about') {
-      res.end('<h1>About page</h1>');
+      fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+         if (err) throw err;
+         res.writeHead(200, { 'Content-Type': 'text/html' });
+         res.end(content);
+      })
    }
 });
 
