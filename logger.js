704e7ee58498bc69f5c4e5f85686ebdb66cfea7b
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-26 13:18:40 -0300

    creating uuid

---
+++ logger.js
@@ -0,0 +1,5 @@
+const EventEmitter = require('events');
+const uuid = require('uuid');
+
+console.log('creating uuid v4', uuid.v4);
+
---
+++ reference/uuid_demo.js
@@ -0,0 +1,3 @@
+const uuid = require('uuid');
+
+console.log('creating uuid v4', uuid.v4());
